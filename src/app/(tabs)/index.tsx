b78536d03fb8a4339df07de58f06cb3f1737a788
Qadeer Ahmad <qadeerahmad294gb@gmail.com>
2025-03-04 14:16:08 +0500

    add all template files and configure the project

---
+++ src/app/(tabs)/index.tsx
@@ -0,0 +1,19 @@
+import { View } from 'react-native';
+
+
+import products from '../../../assets/data/products';
+import { ProductListItems } from '@/src/components/ProductListItems';
+
+
+
+
+
+export default function MenuScreen() {
+  return (
+    <View>
+      <ProductListItems product={products[0]} />
+      <ProductListItems product={products[4]} />
+    </View>
+  );
+}
+
