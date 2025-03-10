31b0ae86f4e3c4e25e060dddb3e0afd2120ed399
Qadeer Ahmad <qadeerahmad294gb@gmail.com>
2025-03-10 23:37:55 +0500

    make add to cart(functionality), contextAPI, cart display

--- src/app/(tabs)/menu/[id].tsx
+++ src/app/(tabs)/menu/[id].tsx
@@ -4,19 +4,26 @@ import { Stack, useLocalSearchParams } from 'expo-router'
 import products from '@/assets/data/products'
 import { useState } from 'react'
 import Button from '@/src/components/Button'
+import { useCart } from '@/src/providers/CartProvider'
+import { PizzaSize } from '@/src/types'
 
 
 
-const sizes = ['S', 'M', 'L', 'XL']
+const sizes: PizzaSize[] = ['S', 'M', 'L', 'XL']
 
 const ProductDetailsScreen = () => {
   const { id } = useLocalSearchParams()
+  const { items, addItem } = useCart()
 
-  const [selectedSize, setSelectedSize] = useState('M')
+  const [selectedSize, setSelectedSize] = useState<PizzaSize>('M')
 
   const product = products.find((p) => p.id.toString() === id)
   const addToCart = () => {
-    console.warn('Added to cart, size', selectedSize)
+    if (!product) {
+      return
+    }
+
+    addItem(product, selectedSize)
   }
 
   if (!product) {
--- src/app/(tabs)/menu/index.tsx
+++ src/app/(tabs)/menu/index.tsx
@@ -1,8 +1,9 @@
-import { View, FlatList } from 'react-native';
+import { View, FlatList, Platform } from 'react-native';
 
 
 import products from '@assets/data/products';
 import { ProductListItems } from '@components/ProductListItems';
+import { StatusBar } from 'expo-status-bar';
 
 
 
@@ -14,7 +15,8 @@ export default function MenuScreen() {
     <View>
       {/* <ProductListItems product={products[0]} />
       <ProductListItems product={products[4]} /> */}
-      <FlatList data={products} renderItem={({item})=><ProductListItems product={item}  />} numColumns={2} contentContainerStyle={{gap:10, padding:10,}} columnWrapperStyle={{gap:10}} />
+      <FlatList data={products} renderItem={({ item }) => <ProductListItems product={item} />} numColumns={2} contentContainerStyle={{ gap: 10, padding: 10, }} columnWrapperStyle={{ gap: 10 }} />
+      <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
     </View>
   );
 }
