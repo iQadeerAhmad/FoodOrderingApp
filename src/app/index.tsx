5397359c3e07d52a939fbadcdc25deadaba429cb
Qadeer Ahmad <qadeerahmad294gb@gmail.com>
2025-04-05 00:15:56 +0500

    add authentication restriction of using admin screen via user side

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -6,7 +6,7 @@ import { useAuth } from '../providers/AuthProvider';
 import { supabase } from '../lib/supabase';
 
 const index = () => {
-    const { session, loading } = useAuth()
+    const { session, loading, isAdmin } = useAuth()
 
     if (loading) {
         return (
@@ -17,6 +17,10 @@ const index = () => {
     if (!session) {
         return <Redirect href={'/sign-in'} />
     }
+    if (!isAdmin) {
+        return <Redirect href={'/(user)'} />
+
+    }
 
     return (
         <View style={{ flex: 1, justifyContent: 'center', padding: 10 }}>
@@ -26,9 +30,7 @@ const index = () => {
             <Link href={'/(admin)'} asChild>
                 <Button text="Admin" />
             </Link>
-            <Link href={'sign-in'} asChild>
-                <Button text="Sign in" />
-            </Link>
+
             <Button text="Sign out" onPress={() => { supabase.auth.signOut() }} />
         </View>
     );
