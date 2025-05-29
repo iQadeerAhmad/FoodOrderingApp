1b8a2da8552adf927a967d1c54247cb12c4e36b3
Qadeer Ahmad <qadeerahmad294gb@gmail.com>
2025-05-30 00:59:55 +0500

    make order items list to latest first on both user and admin side

--- src/api/orders/index.ts
+++ src/api/orders/index.ts
@@ -10,7 +10,7 @@ export const useAdminOrderLIst = ({ archived = false }) => {
     return useQuery({
         queryKey: ['orders', { archived }],
         queryFn: async () => {
-            const { data, error } = await supabase.from('orders').select('*').in('status', statuses)
+            const { data, error } = await supabase.from('orders').select('*').in('status', statuses).order('created_at', { ascending: false })
             if (error) {
                 throw new Error(error.message)
             }
@@ -27,7 +27,7 @@ export const useMyOrderLIst = () => {
         queryKey: ['orders', { userId: id }],
         queryFn: async () => {
             if (!id) return null;
-            const { data, error } = await supabase.from('orders').select('*').eq('user_id', id)
+            const { data, error } = await supabase.from('orders').select('*').eq('user_id', id).order('created_at', { ascending: false })
             if (error) {
                 throw new Error(error.message)
             }
